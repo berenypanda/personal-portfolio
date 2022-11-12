31999846a0041e58477d0005ce8750eb6131c46f
Berenshia <apundaberen@gmail.com>
2022-11-12 14:48:51 -0600

    Create mobile nav togglers

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileToggler = document.querySelector('.navbar-toggler');
+    const navigation = document.querySelector(mobileToggler.getAttribute('data-target'));
+
+    let dropdownToggle = document.querySelectorAll('.dropdown-toggle');
+    
+    mobileToggler.addEventListener('click', () => navigation.classList.toggle('collapse'))
+
+    dropdownToggle.forEach((dropdown) => {
+        dropdown.addEventListener('click', (toggler) => {
+            let dropdownElement = dropdown.parentElement;
+            dropdownElement.classList.toggle('collapse');
+        })
+    })
+});
